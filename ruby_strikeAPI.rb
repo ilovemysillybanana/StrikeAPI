9be7b69fcf0ea9c349b1f5162fc27cfb6fb4304f
Jose Tobar <ilovemysillybanana@gmail.com>
2015-03-08 15:49:47 -0400

    Added links for libraries in use

--- ruby_strikeAPI.rb
+++ ruby_strikeAPI.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
+# http://ruby-doc.org/stdlib-2.0.0/libdoc/open-uri/rdoc/OpenURI.html
 require 'open-uri'
+# https://github.com/flori/json
 require 'json'
+# http://stackoverflow.com/questions/9008847/what-is-difference-between-p-and-pp
 require 'pp'
 
 class Ruby_strikeAPI
