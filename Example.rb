5b66e458c56caaa66e6ca1c16dec3975df5c6679
Jose Tobar <ilovemysillybanana@gmail.com>
2015-03-13 19:51:38 -0400

    Fixed example code and comments

--- Example.rb
+++ Example.rb
@@ -4,9 +4,9 @@ require './ruby_strikeAPI.rb'
 #this is how you instantiate the class
 instantiateExample = Ruby_strikeAPI.new
 
-exampleQuery = instantiateExample.constructURL('B425907E5755031BDA4A8D1B6DCCACA97DA14C04')#in quotations is what your query should be
+#in quotations is what your query should be
+exampleQuery = instantiateExample.constructURL('B425907E5755031BDA4A8D1B6DCCACA97DA14C04')
 
 #getting the output is done like this:
-
 exampleGetInformation = instantiateExample.getFileInformationAllInfo(1)
 puts exampleGetInformation
--- README.md
+++ README.md
@@ -5,4 +5,12 @@ To get more information about Strike go here:
 http://getstrike.net/api/
 
 There are different parameters to get different information.
-> 0 will 
+
+> 0 is a special parameter
+
+0 will always result in the returning of an the first result which is the statistics of the query.
+However because the statistics are limited, usually passing in 0 as a paramater will result in being given 0.
+Passing in paramaters is optional.
+
+There is an example class included.
+
